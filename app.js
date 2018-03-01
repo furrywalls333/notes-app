9ff18dc72595fa9ea0ee34b60251b464f83ef78f
Jeremy Bushey <jeremy.j.bushey@raytheon.com>
2018-03-01 15:47:22 -0500

    initial implementation of addNote functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
+
 if (command === 'add') {
   notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
